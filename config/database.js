6df4d79057385284e06ff29bbd95dedd9964b43b
coder787 <43294866+coder787@users.noreply.github.com>
2018-09-29 19:03:38 +0100

    secure db string using env
    added mongo db connection string to env variable and excluded it from upload  so it can be used in database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
 // config/database.js
 module.exports = {
 
- //   'url' : 'mongodb://colin777:1234@cluster0-shard-00-02-zpmkl.mongodb.net:27017' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-     'url' : 'mongodb://colin777:1234@cluster0-shard-00-02-zpmkl.mongodb.net:27017,cluster0-shard-00-02-zpmkl.mongodb.net:27017,cluster0-shard-00-02-zpmkl.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+    'url': process.env.MONGOLAB_URI
 
 };
--- server.js
+++ server.js
@@ -14,8 +14,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
+require('dotenv').config() // used to load environment variables from .env file
+
 var configDB = require('./config/database.js');
 
+console.log("process.env.MONGOLAB_URI is now", process.env.MONGOLAB_URI);
 // configuration ===============================================================
 mongoose.connect(configDB.url, {
     useMongoClient: true
@@ -23,6 +26,8 @@ mongoose.connect(configDB.url, {
 //mongoose.createConnection(configDB.url);
 
 require('./config/passport')(passport); // pass passport for configuration
+
+
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -53,22 +58,22 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 // middleware to pass user to all templates
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 
     if (req.user == undefined) {
-        req.user = null; 
+        req.user = null;
     }
 
     res.locals.user = req.user;
     next();
-  });
+});
 
 app.use(express.static(__dirname + '/public'));
 
 process.on('unhandledRejection', (reason, p) => {
     console.log('Unhandled Rejection at:', p, 'reason:', reason);
     // application specific logging, throwing an error, or other logic here
-  });
+});
 
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
